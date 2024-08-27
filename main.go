4e6c2144682652f42bc18546dc11ab7bab7b1be4
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-08-27 08:35:31 +0300

    fix: remove duplicate code

--- main.go
+++ main.go
@@ -31,37 +31,3 @@ func main() {
 
     r.Run(":" + port) // listen and serve on 0.0.0.0:8080
 }
-
-package main
-
-import (
-	"log"
-	"os"
-
-	"mobile-customer-portal-server/handlers"
-	"mobile-customer-portal-server/utils"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-)
-
-func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatalf("Error loading .env file")
-    }
-
-    r := gin.Default()
-    utils.ConnectDatabase()
-
-    r.POST("/login", handlers.Login)
-    r.POST("/reset-password", handlers.ResetPassword)
-    r.POST("/verify-otp", handlers.VerifyOTP)
-
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
-
-    r.Run(":" + port) // listen and serve on 0.0.0.0:8080
-}
