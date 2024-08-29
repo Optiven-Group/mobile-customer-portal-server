484871fac4bc98abf1a4866aa68f1e1ba9bcb5cd
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-08-29 15:49:16 +0300

    Add .env to .gitignore

--- utils/db.go
+++ utils/db.go
@@ -5,14 +5,18 @@ import (
 	"log"
 	"mobile-customer-portal-server/models"
 	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+// Global variables to hold the database connections
 var DefaultDB *gorm.DB
 var CustomerPortalDB *gorm.DB
 
+// ConnectDatabase establishes connections to the default and customer portal databases
 func ConnectDatabase() {
+    // Construct the DSN (Data Source Name) for the default database using environment variables
     defaultDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
         os.Getenv("DB_USER"),
         os.Getenv("DB_PASSWORD"),
@@ -21,6 +25,7 @@ func ConnectDatabase() {
         os.Getenv("LOGISTICS_DB"),
     )
 
+    // Construct the DSN (Data Source Name) for the customer portal database using environment variables
     customerPortalDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
         os.Getenv("DB_USER"),
         os.Getenv("DB_PASSWORD"),
@@ -31,15 +36,18 @@ func ConnectDatabase() {
 
     var err error
 
+    // Open a connection to the default database
     DefaultDB, err = gorm.Open(mysql.Open(defaultDSN), &gorm.Config{})
     if err != nil {
-        log.Fatalf("Failed to connect to default database: %v", err)
+        log.Fatalf("Failed to connect to default database: %v", err) // Log and exit if the connection fails
     }
 
+    // Open a connection to the customer portal database
     CustomerPortalDB, err = gorm.Open(mysql.Open(customerPortalDSN), &gorm.Config{})
     if err != nil {
-        log.Fatalf("Failed to connect to customer portal database: %v", err)
+        log.Fatalf("Failed to connect to customer portal database: %v", err) // Log and exit if the connection fails
     }
 
+    // Automatically migrate the schema for the User and Group models to the customer portal database
     CustomerPortalDB.AutoMigrate(&models.User{}, &models.Group{})
 }
